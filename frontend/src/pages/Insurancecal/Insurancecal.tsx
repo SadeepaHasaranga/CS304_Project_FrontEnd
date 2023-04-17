ad877d4765b0f215f37e42f35979d2d5f452f97d
SadeepaHasaranga <sadeepa996@gmail.com>
2023-04-17 22:01:54 +0530

    insurance done

--- frontend/src/pages/Insurancecal/Insurancecal.tsx
+++ frontend/src/pages/Insurancecal/Insurancecal.tsx
@@ -4,13 +4,32 @@ import Callist from '../../Components/callist'
 import Radio from '../../Components/Radio'
 
 const Insurancecal = () => {
-  const [value, setValue] = useState<any>(0)
+  const [value, setValue] = useState<any>();
+  const [model, setModel] = useState<any>();
+  const [mileage, setMileage] = useState<any>();
+  const [year, setYear] = useState<any>();
+  const[result,setResult] = useState<any>();
   const [options, setOptions] = useState<any>({
     dsfs:false,
 
   })
+ const handleValueChange = (e:any) => {
+  setValue(Number(e.target.value));
+};
+const handleModelChange = (e:any) => {
+  setModel(e.target.value);
+};
+const handleMileageChange = (e:any) => {
+  setMileage(Number(e.target.value));
+};
+const handleYearChange = (e:any) => {
+  setYear(Number(e.target.value));
+};
+ 
 
-  const calcuator=()=>{
+
+
+const calcuator=()=>{
     return value*0.01+options.dsfs*2000
   }
   return (
@@ -21,24 +40,24 @@ const Insurancecal = () => {
         <Callist />
       </div>
       <div>
-        <label htmlFor="Bname" className="font-bold">
+        <label  className="font-bold">
           Enter your valivation
         </label>
         <input
           type="text"
-          id="Bname"
+          value={value}
           className="input-area1 font-bold m-4"
           placeholder="Vehicle value"
           style={{ marginLeft: "70px" }}
         />
       </div>
       <div>
-        <label htmlFor="Bname" className="font-bold">
+        <label className="font-bold">
           Brand
         </label>
         <input
           type="text"
-          id="Bname"
+          value={model}
           className="input-area2 font-bold text-#fffbeb m-4"
           placeholder="Enter Brand"
           style={{ marginLeft: "180px" }}
@@ -49,8 +68,8 @@ const Insurancecal = () => {
           Year
         </label>
         <input
-          type="text"
-          id="Bname"
+          type="number"
+          value={year}
           className="input-area3 font-bold m-4"
           placeholder="Enter year"
           style={{ marginLeft: "190px" }}
@@ -64,7 +83,8 @@ const Insurancecal = () => {
           Mileage
         </label>
         <input
-          type="text"
+          type="number"
+          value={mileage}
           id="Bname"
           className="input-area4 font-bold m-4"
           placeholder="Mileage"
@@ -73,6 +93,7 @@ const Insurancecal = () => {
       </div>
 
       <button className="cal-btn">Calculate</button>
+      <h1 className='font-bold'>Your annul insurance fee:{result}</h1>
     </div>
   );
 }
--- frontend/src/pages/SellingResult/SeliingResult.tsx
+++ frontend/src/pages/SellingResult/SeliingResult.tsx
@@ -1,38 +1,39 @@
 import React, { useEffect, useState } from "react";
 import SellingCard from "./Components/SellingCard";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const SeliingResult = () => {
   const sampleData = [
     {
       id: 1,
-      model: "gfff",
-      price: "dkdkd",
-      condition: "ssihfsef",
-      milleage: "dsfsf",
-      name: "amal",
-      telephone: "bvdscsdcsd",
+      model: "Aqua",
+      price: "5000000",
+      condition: "Recondition",
+      milleage: "50000km",
+      name: "Sadeepa",
+      telephone: "0776828388",
       image:
-        "https://www.google.com/search?q=photos&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDlJWk4J7-AhUawzgGHdy4C8sQ_AUoAXoECAEQAw&biw=1366&bih=617&dpr=1#imgrc=jwXiFOBaYZPt2M",
+        "https://www.google.com/search?q=toyota+aqua+in+sri+lanka&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiLu8u6uab-AhUMTWwGHQlkBokQ_AUoAXoECAEQAw&biw=1366&bih=617&dpr=1#imgrc=7qoCWUbLGo2xtM",
     },
-    {
-      id: 2,
-      model: "gfff",
-      price: "dkdkd",
-      condition: "ssihfsef",
-      milleage: "dsfsf",
-      name: "kamal",
-      telephone: "bvdscsdcsd",
-      image:
-        "https://www.google.com/search?q=photos&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDlJWk4J7-AhUawzgGHdy4C8sQ_AUoAXoECAEQAw&biw=1366&bih=617&dpr=1#imgrc=jwXiFOBaYZPt2M",
-    },
+    // {
+    //   id: 2,
+    //   model: "Aqua",
+    //   price: "4500000",
+    //   condition: "Recondition",
+    //   milleage: "1000000",
+    //   name: "kamal",
+    //   telephone: "0772836469",
+    //    image:
+    //    "https://www.google.com/search?q=photos&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDlJWk4J7-AhUawzgGHdy4C8sQ_AUoAXoECAEQAw&biw=1366&bih=617&dpr=1#imgrc=jwXiFOBaYZPt2M",
+    // },
   ];
 
   const [vehicle, setVehicle] = useState<any>([]);
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_BASE_URL}api/v1/vehicle/getVehicles`)
+      .get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/vehicle/getVehicles`)
       .then((res) => setVehicle(res.data))
       .catch((err) => console.error(err));
   }, []);
@@ -42,20 +43,26 @@ const SeliingResult = () => {
   return (
     <div className="ml-[80px]">
       <div className="grid grid-flow-row grid-cols-2 gap-4">
-        {sampleData?.map((a: any) => (
+        {vehicle?.map((a: any) => (
           <>
             <SellingCard
               model={a.model}
               price={a.price}
-              condition={a.condition}
-              milleage={a.milleage}
+              condition={a.cndition}
+              milleage={a.mileage}
               name={a.name}
-              telephone={a.telephone}
+              telephone={a.phonenum}
               image={a.image}
+              id={a.vehicleID}
             />
           </>
         ))}
         {/* ))} */}
+      </div>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <Link to="/Support">Loans</Link>
+        </button>
       </div>
     </div>
   );
